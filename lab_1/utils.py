9fc2332ce71894d9112f347f0dd280a7aeb5dc7e
Kirill Trofimov <kaha2015-2016@mail.ru>
2024-05-06 17:02:22 +0400

    Finished the GUI. Completed the functions.

--- lab_1/utils.py
+++ lab_1/utils.py
@@ -2,47 +2,75 @@ import json
 from pathlib import Path
 import sys
 
-from PyQt5.QtWidgets import QCalendarWidget, QFileDialog, QApplication, QWidget, QPushButton, QGridLayout, QLabel, QTextEdit
-from PyQt5.QtCore import QDate
+from PyQt5.QtWidgets import QFileDialog, QApplication, QWidget, QPushButton, QGridLayout, QLabel, QTextEdit
 
 
-def read_key(file_path: str) -> dict:
+def read_key(file_path: str) -> dict[str, str]:
+    """Reads the file with the decryption key"""
     try:
-        key = Path(file_path)
-        with open(key, "r", encoding = "utf-8") as file:
+        file_path = Path(file_path)
+        with open(file_path, "r", encoding = "utf-8") as file:
             data = json.load(file)
             return data
     except:
         return None
 
 def read_text(file_path: str) -> str:
+    """Reads the text from the file"""
     try:
-        text = Path(file_path)
-        with open(text, "r", encoding = "utf-8") as file:
+        file_path = Path(file_path)
+        with open(file_path, "r", encoding = "utf-8") as file:
             src = file.read()
             return src
-    except Exception: 
-        return Exception
+    except:
+        return None
 
-def descryption(a: str, slovar: dict) -> str:
-    if a == None or slovar == None:
-        print("Отсутствует текст либо ключ дешифрования!")
-        return None
-    try: 
-        for k, v in slovar.items():
-            a = a.replace(k, v)
-    except: 
-        return None
-    return a.upper()
+def descryption(text: str, key: dict[str, str]) -> str:
+    """Decrypts text by key"""
+    src = ""
+    if text == None or key == None:
+        return "Отсутствует текст либо ключ шифрования!"
+    try:
+        for symbol in text:
+            try:
+                src += key[symbol]
+            except:
+                src += symbol
+    except:
+        return "Oh no!"
+    return src
 
-def encryption(a: str, slovar: dict) -> str:
-    if a == None or slovar == None:
-        print("Отсутствует текст либо ключ шифрования!")
-        return None
-    a = a.upper()
+def encryption(text: str, key: dict[str, str]) -> str:
+    """Encrypts text by key"""
+    src = ""
+    if text == None or key == None:
+        return "Отсутствует текст либо ключ шифрования!"
+    text = text.upper()
+    key = list(key.keys())
+    value = list(key.values())
     try: 
-        for k, v in slovar.items():
-            a = a.replace(k, v)
+        for symbol in text:
+            try:
+                src += key[value.index(symbol)]
+            except:
+                src += symbol
     except: 
-        return None
-    return a
+        return "Oh no!"
+    return src
+
+def save_frequency_analysis(file_path: str, text: str) -> None:
+    """Saves the character frequency in a json file"""
+    file_path = Path(file_path)
+    dic = dict()
+    
+    for i in text:
+        if i in dic:
+            dic[i] += 1
+        else:
+            dic[i] = 1
+
+    for k in dic.keys():
+        dic[k] = dic[k] / len(text)
+
+    with open(file_path, 'w', encoding = "utf-8") as file:
+        json.dump(dic, file)
--- lab_1/window.py
+++ lab_1/window.py
@@ -1,58 +1,93 @@
 from utils import *
 
+
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+
     def initUI(self):
         self.key_path = QFileDialog.getOpenFileName(self, 'Выберите файл с ключом', filter = "*.json")
-        self.text_path = QFileDialog.getOpenFileName(self, 'Выберите файл с ключом', filter = "*.txt")
+        self.text_path = QFileDialog.getOpenFileName(self, 'Выберите файл с текстом', filter = "*.txt")
 
         self.setWindowTitle('Приложение')
         self.setGeometry(800, 800, 800, 600)
 
-        self.select_new_key = QPushButton('Выбрать новый ключ', self)
-        self.select_new_key.clicked.connect(self.select_key)
+        self.select_new_key_button = QPushButton('Выбрать новый ключ', self)
+        self.select_new_key_button.clicked.connect(self.select_key)
         
-        self.select_new_text = QPushButton('Выбрать новый текст', self)
-        self.select_new_text.clicked.connect(self.select_text)
+        self.select_new_text_button = QPushButton('Выбрать новый текст', self)
+        self.select_new_text_button.clicked.connect(self.select_text)
         
-        self.btn_save_dataset = QPushButton('Дешифровать', self)
-        self.btn_save_dataset.clicked.connect(self.descryption_text)
+        self.descryption_button = QPushButton('Дешифровать', self)
+        self.descryption_button.clicked.connect(self.descryption_text)
 
-        self.btn_split_by_weeks = QPushButton('Зашифровать', self)
-        self.btn_split_by_weeks.clicked.connect(self.encryption_text)
+        self.encryption_button = QPushButton('Зашифровать', self)
+        self.encryption_button.clicked.connect(self.encryption_text)
         
         self.label1 = QLabel('Исходный текст', self)
         self.label2 = QLabel('Преобразованный текст', self)
 
-        self.textEdit1 = QTextEdit(read_text(self.text_path[0]), self)
-        self.textEdit2 = QTextEdit("", self)
+        self.text_edit1 = QTextEdit(read_text(self.text_path[0]), self)
+        self.text_edit2 = QTextEdit("", self)
+
+        self.swap_button = QPushButton('Преобразованный -> Исходный', self)
+        self.swap_button.clicked.connect(self.swap_texts)
+
+        self.save_button = QPushButton('Сохранить преобразованный текст', self)
+        self.save_button.clicked.connect(self.save_file)
+
+        self.analysis1_button = QPushButton('Сохранить частоту символов исходного текста', self)
+        self.analysis1_button.clicked.connect(self.frequency_analysis1)
+
+        self.analysis2_button = QPushButton('Сохранить частоту символов преобразованного текста', self)
+        self.analysis2_button.clicked.connect(self.frequency_analysis2)
         
         vbox = QGridLayout()
         
-        vbox.addWidget(self.select_new_key, 0, 0)
-        vbox.addWidget(self.select_new_text, 0, 1)
-        vbox.addWidget(self.textEdit1, 1, 0)
-        vbox.addWidget(self.textEdit2, 1, 1)
+        vbox.addWidget(self.select_new_key_button, 0, 0)
+        vbox.addWidget(self.select_new_text_button, 0, 1)
+        vbox.addWidget(self.text_edit1, 1, 0)
+        vbox.addWidget(self.text_edit2, 1, 1)
         vbox.addWidget(self.label1, 2, 0)
         vbox.addWidget(self.label2, 2, 1)
-        vbox.addWidget(self.btn_save_dataset, 3, 0)
-        vbox.addWidget(self.btn_split_by_weeks, 3, 1)
+        vbox.addWidget(self.descryption_button, 3, 0)
+        vbox.addWidget(self.encryption_button, 3, 1)
+        vbox.addWidget(self.swap_button, 4, 0)
+        vbox.addWidget(self.save_button, 4, 1)
+        vbox.addWidget(self.analysis1_button, 5, 0)
+        vbox.addWidget(self.analysis2_button, 5, 1)
         self.setLayout(vbox)
 
     def descryption_text(self):
-        self.textEdit2.setText(descryption(self.textEdit1.toPlainText(), read_key(self.key_path[0])))
+        self.text_edit2.setText(descryption(self.text_edit1.toPlainText(), read_key(self.key_path[0])))
 
     def encryption_text(self):
-        self.textEdit2.setText(encryption(self.textEdit1.toPlainText(), read_key(self.key_path[0])))
+        self.text_edit2.setText(encryption(self.text_edit1.toPlainText(), read_key(self.key_path[0])))
 
     def select_text(self):
-        self.text_path = QFileDialog.getOpenFileName(self, 'Выберите файл с ключом', filter = "*.txt")
-        self.textEdit1.setText(read_text(self.text_path[0]))
+        self.text_path = QFileDialog.getOpenFileName(self, 'Выберите файл с текстом', filter = "*.txt")
+        self.text_edit1.setText(read_text(self.text_path[0]))
 
     def select_key(self):
         self.key_path = QFileDialog.getOpenFileName(self, 'Выберите файл с ключом', filter = "*.json")
+
+    def swap_texts(self):
+        self.text_edit1.setText(self.text_edit2.toPlainText())
+        self.text_edit2.setText("")
+
+    def save_file(self):
+        fname = QFileDialog.getSaveFileName(self, 'Сохранить преобразованный текст')
+        with open(Path(fname[0]), 'w', encoding = "utf-8") as file:
+            file.write(self.text_edit2.toPlainText())
+
+    def frequency_analysis1(self):
+        fname = QFileDialog.getSaveFileName(self, 'Сохранить частоту символов исходного текста')
+        save_frequency_analysis(fname[0], self.text_edit2.toPlainText())
+
+    def frequency_analysis2(self):
+        fname = QFileDialog.getSaveFileName(self, 'Сохранить частоту символов преобразованного текста')
+        save_frequency_analysis(fname[0], self.text_edit2.toPlainText())
 
     
 
